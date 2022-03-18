a523e422ab2b0ed45e25251f9c959d4d488c8121
Sajadullah <88251767+sajadu@users.noreply.github.com>
2022-03-18 09:22:32 +0500

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,24 +1,16 @@
 <?php
+if(!empty($_POST["send"])) {
+	$name = $_POST["name"];
+	$email = $_POST["email"];
+	$subject = $_POST["text"];
+	$content = $_POST["form-control"];
 
-
- if(isset($_POST['submit'])){
-    $to = "freelancersajad@gmail.com"; // this is your Email address
-    $from = $_POST['email']; // this is the sender's Email address
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $subject = "Form submission";
-    $subject2 = "Copy of your form submission";
-    $message = $name . " " . $email . $subject. " wrote the following:" . "\n\n" . $_POST['message'];
-    $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
-
-    $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
-    mail($to,$subject,$message,$headers);
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
-    }
-
-  
- 
+	$toEmail = "freelancersajad@gmail.com";
+	$mailHeaders = "From: " . $name . "<". $email .">\r\n";
+	if(mail($toEmail, $subject, $content, $mailHeaders)) {
+	    $message = "Your contact information is received successfully.";
+	    $type = "success";
+	}
+}
+require_once "contact-view.php";
 ?>
